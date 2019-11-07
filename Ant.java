2e3cd495d35b3c57edadc253755b5cf1e694b077
Will Donaldson <will.donaldson97@gmail.com>
2019-11-06 22:38:19 -0500

    yeeet

--- Ant.java
+++ Ant.java
@@ -7,11 +7,11 @@ public class Ant {
 
 	//represent unvisited cities as a bitset. if a city is unvisited, it is a zero (all cities start as zeros)
 	//then once it is visited it becomes a one.
-	private BitSet univisited_cities = new BitSet(Reader.num_cities - 1);
+	private BitSet univisited_cities;
 	private int starting_city;
 
 	public Ant() {
-
+		univisited_cities = new BitSet(Reader.num_cities - 1);
 	}
 
 	public static decide() {
