ffbed854cff94ee20035141f530ad5551668408c
tomjones-4 <tdjones@bowdoin.edu>
2019-11-06 20:37:51 -0500

    Clean up reader

--- Reader.java
+++ Reader.java
@@ -1,30 +1,17 @@
-/* Copied Reader.java over from GA/PBIL project because I figured this
-   would be a good starting point.
-*/
-
-/* Important information from test file are the coordinates of the cities.
-   I'm thinking the best way to store these is to have an array of lists callied cities.
+/* This class reads in information from the test problems and stores it in a 2D in array.
    Array is of length n, where n = num cities, and each list is length 2.
-   cities[i][0] is the x-coord of city i and cities[i][1] is the y-coord of city i.
-   The coordinates start after a line that says NODE_COORD_SECTION
-   The information stored in each line is the number of the city, then x, then y.
+   city_coords[i][0] is the x-coord of city i and city_coords[i][1] is the y-coord of city i.
 */
 
-//import java.util.Scanner;
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
 
 public class Reader {
 
-	private StringBuilder file_string = new StringBuilder();
-	private String output_string = new String();
-	private int number_of_variables;
-    private int number_of_clauses;
     private int num_cities;
     private int[][] city_coords;
-	private List<Integer[]> clause_list = new ArrayList<Integer[]>();
 
-	//Get information from the problem file like number of variables and number of clauses
 	public Reader(String file_path){
 		File file = new File(file_path);
 		//Read the file in as a string
@@ -33,84 +20,53 @@ public class Reader {
 			while (file_scan.hasNextLine()){
                 String line = file_scan.nextLine();
                 if (line.substring(0, 3).equals("DIM")) { //this line contains number of cities in problem
-                    //int len_line = line.length(); //debugging
-                    //System.out.println(len_line); //debugging
                     num_cities = Integer.parseInt(line.substring(12)); //gets number of cities as int
-                    System.out.println(num_cities); //debugging
                     city_coords = new int[num_cities][2];
                 }
-                else if (line.substring(0, 4).equals("NODE")) { //rest of lines are city coords
+                else if (line.substring(0, 4).equals("NODE")) {
+                    //rest of lines are city coords
                     String string_coords = file_scan.nextLine();
+                    //while line is still coords and not end of file (EOF)
                     while (!string_coords.equals("EOF")) {
+                        //each line has 3 numbers separated by spaces
                         String[] split_coords = string_coords.split(" ");
+                        //first number is city number, subtract 1 to properly index in city_coords array
                         int city_num = Integer.parseInt(split_coords[0]) - 1;
-                        //int x_coord = new BigDecimal(split_coords[1]).intValue();
-                        //int y_coord = new BigDecimal(split_coords[2]).intValue();
+                        //second number is x_coord, parseDouble since sometimes scientific notation is used
                         int x_coord = (int) Double.parseDouble((split_coords[1]));
+                        //third number is y_coord
                         int y_coord = (int) Double.parseDouble((split_coords[2]));
                         city_coords[city_num][0] = x_coord;
                         city_coords[city_num][1] = y_coord;
                         string_coords = file_scan.nextLine();
                     }
                 }
-                //System.out.println(line);
-				//else {
-				//	this.file_string.append(line + "\n");
-				//}
             }
-            for (int i = 1; i <= num_cities; i++) {
+            // debugging
+            /*
+            for (int i = 1; i <= num_cities; i++) { //this loop is for debugging
                 System.out.println("City number: " + i + ": " + city_coords[i-1][0] + ", " + city_coords[i-1][1]);
             }
-			//this.output_string = this.file_string.toString();
+            */
 		}
 		catch (FileNotFoundException e) {
 			e.printStackTrace();
         }
-        /*
-		//Extract data from the file
-		String[] source_ary = this.output_string.split("\n");
-		String first_line = source_ary[0];
-
-		//Splits based on white spaces, even if there are multiple white spaces
-		String[] first_line_ary = first_line.split("\\s+");
-
-		this.number_of_variables = Integer.valueOf(first_line_ary[2]);
-		this.number_of_clauses = Integer.valueOf(first_line_ary[3]);
-	
-		//Represent the clauses as a list of integer arrays.
-		for(int i = 1; i <= this.number_of_clauses; i++) {
-			String clause_string = source_ary[i];
-			String [] clause_string_ary = clause_string.split(" ");
-			Integer[] single_clause_ary = new Integer[clause_string_ary.length - 1];
-			for(int j = 0; j<=clause_string_ary.length-2; j++) {
-				String clause_element = clause_string_ary[j];
-				int numerical_clause_element = Integer.valueOf(clause_element);
-				single_clause_ary[j] = numerical_clause_element;
-			}
-			this.clause_list.add(single_clause_ary);
-        } 
-        */
     }
     
     //main method for testing Reader functionality
+    /*
     public static void main(String[] args) {
         String file = args[0];
         Reader test = new Reader(file);
     }
-	
-	public String getFileString() {
-		return this.output_string;
-	}
+    */
+    
+    public int get_num_cities() {
+        return num_cities;
+    }
 
-	public int getNumberOfVariables() {
-		return this.number_of_variables;
-	}
-
-	public int getNumberOfClauses() {
-		return this.number_of_clauses;
-	}
-
-	public List getClauseList() {
-		return this.clause_list;
-	}
+    public int[][] get_city_coords() {
+        return city_coords;
+    }
 }
