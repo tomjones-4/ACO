33f9aeb94a860ec7b1272ee3c89610749264d2b6
gbenznyc <gbenz@bowdoin.edu>
2019-11-06 21:31:31 -0500

    Adds intialization for distances in paths

--- Paths.java
+++ Paths.java
@@ -5,4 +5,21 @@ public class Paths {
 	public Double[][] city_distances;
 	public Double[][] pheremones;
 
+	public Paths(Double[][] city_cords){
+		city_distances = new Double[city_cords.length][city_cords.length];
+		pheremones = new Double[city_cords.length][city_cords.length];
+
+		//Populate the city_distances with eucleadian distances between city_cords
+		for (int i=0; i < city_cords.length; i++){
+			for (int j=0; j < city_cords.length; j ++){
+				Double y2 = city_cords.get(i).get(1);
+				Double y1 = city_cords.get(j).get(1);
+				Double x2 = city_cords.get(i).get(0);
+				Double x1 = city_cords.get(j).get(0);
+				distance = sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+			}
+		}
+
+	}
+
 }
