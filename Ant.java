a9d0f9f6e5deea41c8d2e080baf277682296c033
tomjones-4 <tdjones@bowdoin.edu>
2019-11-12 14:52:21 -0500

    Start working on ACS

--- Ant.java
+++ Ant.java
@@ -11,11 +11,29 @@ public class Ant {
 	public Ant() {
 		current_city = rand.nextInt(Reader.num_cities);
 		path.add(current_city);
-	}
+    }
+    
+    public class Tour {
+        public Double length;
+        public ArrayList<Integer> cities_visited = new ArrayList<Integer>();
+
+        public Tour(Double length, ArrayList<Integer> cities_visited) {
+            this.length = length;
+            this.cities_visited = cities_visited;
+        }
+
+        public int get_length() {
+            return length;
+        }
+
+        public ArrayList<Integer> get_cities_visited() {
+            return cities_visited;
+        }
+    }
 
 	/* Returns a pair with two values; a double with the total distance of the path and an array list of integers
 	which represents the taken path so far. */
-	public Pair <Double, ArrayList<Integer>> tour() {
+	/*public Pair <Double, ArrayList<Integer>> tour() {
 		for(int i=0; i < Reader.num_cities -1; i++) {
 			this.choose();
 		}
@@ -32,6 +50,27 @@ public class Ant {
 		}
 		Pair<Double, ArrayList<Integer>> ans = new Pair<Double, ArrayList<Integer>>(distance, this.path);
 		return ans;
+    }*/
+    
+    //same method as above commented out one, just uses a class called Tour within Ant class to help clarify
+    public Tour tour() {
+		for(int i=0; i < Reader.num_cities -1; i++) {
+			this.choose();
+		}
+		this.return_to_start();
+		
+		//for debugguing
+		System.out.println(path);
+
+		double distance = 0;
+		for(int i=0; i < path.size()-2; i++) {
+			int current = path.get(i);
+			int next = path.get(i + 1);
+			distance += Runner.PATHS.city_distances[current][next];
+        }
+        
+        Tour tour = new Tour(distance, path);
+        return tour;
 	}
 
 	private void return_to_start() {
@@ -40,6 +79,10 @@ public class Ant {
 		current_city = start;
 	}
 
+    /* I think we need to change choose() method to account for Action Selection Rule in ACS
+       Alternative solution: create a method called greedy_choose() which chooses best city, and leave
+       choose() as is, maybe change its name to random_choose() to clarify.
+    */
 	private void choose() {
 		double [] p_vector = this.create_p_vector();
 		double [] p_choice = new double[p_vector.length];
@@ -71,8 +114,9 @@ public class Ant {
 
 	private double[] create_p_vector() {
 		double [] numerators = new double[Reader.num_cities - 1];
-		double denominator = 0;
-		for (int i=0; i < Reader.num_cities - 1; i++) {
+        double denominator = 0;
+        //I think this should be i < Reader.num_cities because we need to check last city, too
+		for (int i=0; i < Reader.num_cities - 1; i++) { 
 			if (!this.path.contains(i)) {
 				double distance = Paths.city_distances[this.current_city][i];
 				
@@ -82,7 +126,7 @@ public class Ant {
 				
 				double unraised_numerator = heuristic_info * pheremone_level;
 				
-				denominator += unraised_numerator;
+				denominator += unraised_numerator; //I think denominator should be raised, too
 				
 				double raised_heur = Math.pow(heuristic_info, Runner.HEUR_POWER);
 				
@@ -100,7 +144,10 @@ public class Ant {
 				// System.out.println(numerators[i]);
 			}
 		}
-		double [] p_vector = new double[Reader.num_cities-1];
+        double [] p_vector = new double[Reader.num_cities-1];
+        //This is the second time we do a for loop and check to see if each city i is in the path already
+        //I think it will be faster to check whether numerators[i] == 0. If it does, set p_vector[i] = 0.
+        //Otherwise, p_vector[i] = numerators[i] / denominator
 		for (int i=0; i < Reader.num_cities - 1; i++) {
 			if (!this.path.contains(i)) {
 				p_vector[i] = numerators[i] / denominator;
