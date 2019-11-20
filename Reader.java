5909ec8a4b40c70e8e283f203c81844c68793a68
tomjones-4 <tdjones@bowdoin.edu>
2019-11-20 11:43:51 -0500

    Reader works for Berlin

--- Reader.java
+++ Reader.java
@@ -15,62 +15,55 @@ public class Reader {
 
 
 	public Reader(String file_path){
-		File file = new File(file_path);
+        File file = new File(file_path);
+        
 		//read the file in
 		try {	
 			Scanner file_scan = new Scanner(file);
 			while (file_scan.hasNextLine()){
                 String line = file_scan.nextLine();
-                System.out.println("line: " + line); //debugging
-                //this line contains number of cities in problem
-                if (line.substring(0, 3).equals("DIM")) {
-                    System.out.println("DIM line: " + line); //debugging
-                    int index_of_colon = line.indexOf(":");
-                    num_cities = Integer.parseInt(line.substring(index_of_colon + 2)); //gets number of cities as int
-                    //System.out.println("Num cities in Reader.java: " + num_cities); //debugging
-                }
-                else if (line.substring(0, 4).equals("NODE")) {
-                    //rest of lines are city coords
-                    String string_coords = file_scan.nextLine();
-                    //while line is still coords and not end of file (EOF)
-                    while (!string_coords.equals("EOF")) {
-                        //each line has 3 numbers separated by spaces
-                        String[] split_coords = string_coords.trim().split("\\s+");
-                        //first number is city number, subtract 1 to properly index in city_coords array
-                        int city_num = Integer.parseInt(split_coords[0]) - 1;
-                        //second number is x_coord, parseDouble since sometimes scientific notation is used
-                        Double x_coord = Double.parseDouble((split_coords[1]));
-                        //third number is y_coord
-                        Double y_coord = Double.parseDouble((split_coords[2]));
-                        Double[] coords = {x_coord, y_coord};
-                        city_coords.add(coords);
-                        string_coords = file_scan.nextLine();
+                if (line.length() >= 4) {
+
+                    //"DIMENSION" line contains number of cities in problem
+                    if (line.substring(0, 3).equals("DIM")) {
+                        int index_of_colon = line.indexOf(":");
+
+                        //gets number of cities as int
+                        num_cities = Integer.parseInt(line.substring(index_of_colon + 2));
+                    }
+                    
+                    else if (line.substring(0, 4).equals("NODE")) {
+
+                        //rest of lines are city coords
+                        String string_coords = file_scan.nextLine();
+
+                        //while line is still coords and not end of file (EOF)
+                        while (!string_coords.equals("EOF")) {
+
+                            //each line has 3 numbers separated by spaces
+                            String[] split_coords = string_coords.trim().split("\\s+");
+
+                            //first number is city number
+
+                            //second number is x_coord, parseDouble since sometimes scientific notation is used
+                            Double x_coord = Double.parseDouble((split_coords[1]));
+
+                            //third number is y_coord
+                            Double y_coord = Double.parseDouble((split_coords[2]));
+
+                            Double[] coords = {x_coord, y_coord};
+                            city_coords.add(coords);
+                            string_coords = file_scan.nextLine();
+                        }
                     }
                 }
-                else if (line.substring(0, 3).equals("EOF")) {
-                    System.out.println("Breaking"); //debugging
-                    break;
-                }
             }
-            // debugging
-            
-            // for (int i = 1; i <= num_cities; i++) { //this loop is for debugging
-            //     System.out.println("City number " + i + ": " + city_coords.get(i-1)[0] + ", " + city_coords.get(i-1)[1]);
-            // }
             
 		}
 		catch (FileNotFoundException e) {
 			e.printStackTrace();
         }
     }
-    
-    //main method for testing Reader functionality and debugging
-    
-    public static void main(String[] args) {
-        String file = args[0];
-        Reader test = new Reader(file);
-    }
-    
     
     public static int get_num_cities() {
         return city_coords.size();
