3cdb51dddaa9d30e0c0851e73234723c8d937193
tomjones-4 <tdjones@bowdoin.edu>
2019-11-26 14:52:35 -0500

    Clean up

--- Runner.java
+++ Runner.java
@@ -30,7 +30,7 @@ public class Runner {
         System.out.println("If running Ant Colony System:");
         System.out.println("java Runner file type num_ants num_its pher_power heur_power evap_factor wearing_away best_leg");
         System.out.println("    file            = name of the file containing the problem (string)");
-        System.out.println("    colony_type     = Ant Colony System (ACS) or Elitist Ant System (EAS) (String)");
+        System.out.println("    colony_type     = Ant Colony System (ACS) or Elitist Ant System (EAS) (string)");
 	    System.out.println("    num_ants        = number of ants (int)");
 	    System.out.println("    num_its         = number of iterations (int)");
 	    System.out.println("    pher_power      = weight placed on pheremones (double)");
@@ -44,7 +44,7 @@ public class Runner {
         System.out.println("If running Elitist Ant System:");
         System.out.println("java Runner file type num_ants num_its pher_power heur_power evap_factor elitism");
         System.out.println("    file            = name of the file containing the problem (string)");
-        System.out.println("    colony_type     = Ant Colony System (ACS) or Elitist Ant System (EAS) (String)");
+        System.out.println("    colony_type     = Ant Colony System (ACS) or Elitist Ant System (EAS) (string)");
         System.out.println("    num_ants        = number of ants (int)");
         System.out.println("    num_its         = number of iterations (int)");
         System.out.println("    pher_power      = weight placed on pheremones (double)");
